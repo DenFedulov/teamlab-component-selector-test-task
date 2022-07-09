e012e9aa86bcb7af21d524d90f7bec3532316373
DenFedulov <den.fedulov.1999@mail.ru>
2022-07-09 15:16:52 +0300

    Final

--- code.js
+++ code.js
@@ -1,7 +1,83 @@
 'use strict';
 
-const component_selectors = document.querySelectorAll(".component_selector");
+const componentSelectorParents = document.querySelectorAll(".component_selector");
 
-function createComponentSelector(elem) {
-    this.elem = elem;
+// Массив в котором будут храниться все Компонент Селекторы
+const componentSelectors = [];
+
+const selectorFillers = [["Нижний Новгород", "Москва"], ["Екатеринбург", "Бор"], ["Тагил", "Воронеж", "Зеленогорск"]]
+
+class ComponentSelector {
+
+    constructor(parentElem) {
+        this.textElem = undefined;
+        this.optionsListElem = undefined;
+        this.arrowElem = undefined;
+        try {
+            if (parentElem != undefined) {
+                this.parentElem = parentElem;
+            } else throw "parentElem is undefined"
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    init(options) {
+        if (this.parentElem != undefined) {
+            const parent = this.parentElem;
+            const textElem = this.textElem = document.createElement('div');
+            const optionsListElem = this.optionsListElem = document.createElement('ul');
+            const arrowElem = this.arrowElem = document.createElement('img');
+
+            arrowElem.src = 'arrow.png';
+
+            parent.innerHTML = "";
+
+            parent.addEventListener('click', () => {
+                optionsListElem.classList.toggle('on');
+            })
+
+            this.#addOptions(options, optionsListElem);
+
+            textElem.classList.add('text');
+
+            parent.append(textElem);
+            parent.append(optionsListElem);
+            parent.append(arrowElem);
+
+            parent.classList.toggle('on');
+        }
+    }
+
+    #addOptions(options, parent) {
+        try {
+            if (options === undefined) throw "Exception error: no options specified";
+
+            for (const optionName of options) {
+                let elem = document.createElement('li');
+                elem.innerText = optionName;
+
+                elem.addEventListener('click', () => {
+                    this.textElem.innerText = elem.innerText;
+                })
+
+                parent.append(elem);
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
 }
+
+for (const i of componentSelectorParents) {
+    componentSelectors.push(new ComponentSelector(i));
+}
+
+for (const i in componentSelectors) {
+    if (Object.hasOwnProperty.call(componentSelectors, i)) {
+        componentSelectors[i].init(selectorFillers[i]);
+    }
+}
+
